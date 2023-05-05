dc140030be0921b80574c53b88bf9b5f850f13c8
Gabriel Chulim <gabriel.chulim@revenatium.com>
2023-05-05 09:59:49 -0500

    Initial Test for Multi Currency Money [#17-#18. Degenerate Objects]

---
+++ src/test/java/guru/springframework/Dollar.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+public class Dollar {
+    int amount;
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(this.amount * multiplier);
+    }
+}
