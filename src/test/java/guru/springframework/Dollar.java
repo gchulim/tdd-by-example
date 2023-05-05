e2a10568fdf77898843692471cb9d5b33da82b88
Gabriel Chulim <gabriel.chulim@revenatium.com>
2023-05-05 15:23:29 -0500

    Multi Currency Money [#22. Common Funcionality (Create Money class with "amount" property and "equals" method)]

--- src/test/java/guru/springframework/Dollar.java
+++ src/test/java/guru/springframework/Dollar.java
@@ -1,18 +1,11 @@
 package guru.springframework;
 
-public class Dollar {
-    private int amount;
+public class Dollar extends Money{
     public Dollar(int amount) {
         this.amount = amount;
     }
 
     Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        Dollar dollar = (Dollar) o;
-        return amount == dollar.amount;
     }
 }
--- src/test/java/guru/springframework/Franc.java
+++ src/test/java/guru/springframework/Franc.java
@@ -1,19 +1,12 @@
 package guru.springframework;
 
-public class Franc {
-    private int amount;
+public class Franc extends Money {
     public Franc(int amount) {
         this.amount = amount;
     }
 
     Franc times(int multiplier) {
         return new Franc(this.amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        Franc franc = (Franc) o;
-        return amount == franc.amount;
     }
 
 }
