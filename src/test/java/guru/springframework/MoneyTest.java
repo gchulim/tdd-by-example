2e38256991f67692ee9c9d34d6fe965c57b9d25b
Gabriel Chulim <gabriel.chulim@revenatium.com>
2023-05-05 14:57:09 -0500

    Multi Currency Money [#21. Implement Franc Object by test]

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
     @Test
-    void testMultiplication() {
+    void testMultiplicationDollar() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(new Dollar(10), product);
@@ -16,8 +16,23 @@ public class MoneyTest {
     }
 
     @Test
-    void testEquality() {
+    void testEqualityDollar() {
         assertEquals(new Dollar(6), new Dollar(6));
         assertNotEquals(new Dollar(6), new Dollar(8));
+    }
+
+    @Test
+    void testMultiplicationFranc() {
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        assertEquals(new Franc(10), product);
+        product = five.times(3);
+        assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    void testEqualityFranc() {
+        assertEquals(new Franc(6), new Franc(6));
+        assertNotEquals(new Franc(6), new Franc(8));
     }
 }
